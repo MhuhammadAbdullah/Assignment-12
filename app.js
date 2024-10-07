947835ba8cf35c7914c69369b6d4e6c911d2a3e5
Muhammad Abdullah <abaloch0033@gmail.com>
2024-10-07 16:01:54 +0500

    updated

---
+++ app.js
@@ -0,0 +1,26 @@
+//Question: 1
+
+// var toss = Math.floor(Math.random() * 2) + 1;
+// if (toss === 1) {
+//     document.write("It's Heads!");
+// } else {
+//     document.write("It's Tails!");
+// }
+
+//Question: 2
+
+// var dice = Math.floor(Math.random() * 6) + 1;
+// document.write("You rolled a " + dice);
+
+
+//Question: 3
+
+var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$&()";
+var passwordLength = 10;
+var password = "";
+
+for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+}
+document.write("Generated password is: " + password);
